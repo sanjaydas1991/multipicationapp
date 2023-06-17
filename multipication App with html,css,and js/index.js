30b6fca5df837361dbd9a353dab53e9645f96e97
sanjaydas1991 <115581647+sanjaydas1991@users.noreply.github.com>
2023-06-17 16:33:07 +0530

    Add files via upload

---
+++ multipication App with html,css,and js/index.js
@@ -0,0 +1,40 @@
+const num1= Math.ceil(Math.random() * 10);
+const num2= Math.ceil(Math.random() *10);
+
+const questionEl=document.getElementById("question");
+
+const inputEl=document.getElementById("input");
+
+const formEl=document.getElementById("form");
+
+ 
+
+let score= JSON.parse(localStorage.getItem("score"));
+
+const scoreEl = document.getElementById("score")
+
+if (!score)  {
+    score=0;
+}
+ 
+scoreEl.innerText = `score: ${score}`
+
+questionEl.innerText = `what is ${num1} multiply by ${num2}?`;
+
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+    const userAns = inputEl.value;
+    if(userAns === correctAns){
+        score++;
+       updatelocalstorage()
+    }else {
+        score--;
+       updatelocalstorage()
+    }
+});
+function updatelocalstorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
+
